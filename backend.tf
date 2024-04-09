66fa3b99094001789ec3a91501c27ee73cb1b9bd
Lokesh Telange <149856169+lokeshtelange@users.noreply.github.com>
2024-04-09 12:40:30 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "jenkins-cicdbucket"
+    bucket         = "java-bucket"
     key            = "my-terraform-environment/main"
-    region         = "ap-south-1"
-    dynamodb_table = "lokeshtelange-dynamo-db-table"
+    region         = "us-east-1"
+    dynamodb_table = "my-dynamo-db-table"
   }
 }
